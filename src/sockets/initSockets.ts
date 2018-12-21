afce6a513edf51f3decddabea501968b94b27f97
pavolbincik <pavolbinck@gmail.com>
2018-12-21 21:21:54 +0100

    socket.handshake.query.token

--- src/sockets/initSockets.ts
+++ src/sockets/initSockets.ts
@@ -72,7 +72,7 @@ class Sockets {
                             isUnknown: true
                         }
 
-                        console.warn("Worker is not registered in our database.", socket.worker.uuid);
+                        console.warn("Worker is not registered in our database.", socket.handshake.query.token);
                         next();
                     }
 
