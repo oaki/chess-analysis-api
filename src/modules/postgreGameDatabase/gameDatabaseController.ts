8f6a5061a07c471b2835099247193ccbc572c5c1
pavolbincik <pavolbinck@gmail.com>
2019-11-24 21:15:56 +0100

    improve games parsing

--- src/modules/postgreGameDatabase/gameDatabaseController.ts
+++ src/modules/postgreGameDatabase/gameDatabaseController.ts
@@ -7,6 +7,7 @@ import {Move} from "./entity/move";
 import {pgnFileReader} from "../../libs/pgnFileReader";
 import {decodeFenHash} from "../../libs/fenHash";
 import {postgreGameDbConnection} from "../../libs/connectPostgreGameDatabase";
+const {performance} = require("perf_hooks");
 
 const jsMd5 = require("js-md5");
 
@@ -42,83 +43,138 @@ export class GameDatabaseController {
         this.db = await postgreGameDbConnection;
     }
 
-    private findFenInPgn(pgn, fenHash) {
-        const chess = new Chess();
-        const chess2 = new Chess();
-        chess.load_pgn(pgn);
-        const moves = chess.history();
-        const nextFewMoves = [];
-        let counter = 0;
-        let isFound = false;
-        for (let i = 0; i < moves.length; i++) {
-            const move2 = chess2.move(moves[i]);
-            if (move2) {
-                if (!isFound) {
-                    if (decodeFenHash(chess2.fen()) === fenHash) {
-                        isFound = true;
-                    }
-                } else if (counter < 10) {
-                    nextFewMoves.push(move2);
-                    counter++;
-                }
+    private findFenInPgn(pgn: string, fenHash, fenReferenceObj) {
+        const ch1 = new Chess();
+        const regex = /([0-9]{1,3})(\. )([a-zA-Z\-0-8\+\=]{1,6})( )([a-zA-Z\-0-8\+\=]{1,6})/gm;
 
-                if (counter > 10) {
-                    break;
+        let match;
+        let moveCounter = 0;
+        const ms = [];
+        let lastMove;
+
+        while (match = regex.exec(pgn)) {
+
+            lastMove = ch1.move(match[3], {sloppy: true});
+
+            if (fenReferenceObj.ref && moveCounter < 10) {
+                ms.push(lastMove);
+                moveCounter++;
+            }
+
+            if (moveCounter > 10) {
+                break;
+            }
+
+            if (!fenReferenceObj.ref) {
+                const fen = ch1.fen();
+                if (decodeFenHash(fen) === fenHash) {
+                    fenReferenceObj.ref = fen;
+                }
+            }
+
+            lastMove = ch1.move(match[5], {sloppy: true});
+
+            if (fenReferenceObj.ref && moveCounter < 10) {
+                ms.push(lastMove);
+                moveCounter++;
+            }
+
+            if (moveCounter > 10) {
+                break;
+            }
+
+            if (!fenReferenceObj.ref) {
+                const fen = ch1.fen();
+                if (decodeFenHash(fen) === fenHash) {
+                    fenReferenceObj.ref = fen;
                 }
             }
         }
+        // console.log({ms});
 
-        return nextFewMoves.map(move => move.san);
+        // const chess = new Chess();
+        // const chess2 = new Chess();
+        // chess.load_pgn(pgn, {sloppy: false});
+        // const moves = chess.history();
+        // const nextFewMoves = [];
+        // let counter = 0;
+        // let isFound = false;
+        // for (let i = 0; i < moves.length; i++) {
+        //     const move2 = chess2.move(moves[i]);
+        //     if (move2) {
+        //         if (!isFound) {
+        //             if (decodeFenHash(chess2.fen()) === fenHash) {
+        //                 isFound = true;
+        //             }
+        //         } else if (counter < 10) {
+        //             nextFewMoves.push(move2);
+        //             counter++;
+        //         }
+        //
+        //         if (counter > 10) {
+        //             break;
+        //         }
+        //     }
+        // }
+
+        return ms.map(move => move.san);
     }
 
     async get(props: GetProps) {
-
-        console.log(props);
-        // const normalizedFen = PositionService.normalizeFen(props.fen);
-        // const moveEntity = await this.db.getRepository(Game)
-        //     .createQueryBuilder("game")
-        //     .innerJoin("move")
-        //     .where("(move.fen = :fen)")
-        //     .setParameters({fen: props.fen})
-        //     .orderBy("game.whiteElo", "DESC")
-        //     .getMany();
+        const p1 = performance.now();
 
         const fenHash = decodeFenHash(props.fen);
 
-        const query = this.db
-            .getRepository(Game)
-            .createQueryBuilder("game")
-            .where(qb => {
-                const moveQb =
-                    qb.subQuery()
-                        .select("move.id")
-                        .from(Move, "move")
-                        .where("move.fenHash = :fenHash")
-                        .getQuery();
-                return "game.id IN " + moveQb;
-            }).setParameter("fenHash", fenHash);
-        // ORDER BY "game"."result"='1-0' DESC,"game"."result"='1/2-1/2' DESC LIMIT 50
+        const move = await this.db
+            .getRepository(Move)
+            .createQueryBuilder("move")
+            .select("id")
+            .where({fenHash}).getRawOne();
 
-        if (props.side === "w") {
-            query.addOrderBy("game.coefW", "ASC");
+        const games = await this.db.manager.query(`
+        SELECT game.* FROM game WHERE game.id IN 
+            (
+                SELECT 
+                    game_moves_move."gameId" 
+                FROM 
+                    game_moves_move 
+                WHERE 
+                    game_moves_move."moveId" = ${move.id} 
+                ORDER BY ${props.side === "w" ? "game_moves_move.cw" : "game_moves_move.cb"} LIMIT 5
+            )
+        `);
 
-        } else {
-            query.addOrderBy("game.coefW", "ASC");
-        }
-
-        const games: Game[] = await query
-            .limit(5)
-            .getMany();
 
         if (games.length === 0) {
             throw Boom.notFound();
         }
 
-        return {
+        const p2 = performance.now();
+
+        let fenReference = {};
+        const result = {
             games: games.map((game) => {
-                return {...game, fewNextMove: this.findFenInPgn(game.pgn, fenHash)}
+                console.log({fenReference});
+                return {
+                    id: game.id,
+                    white: game.white,
+                    black: game.black,
+                    whiteElo: game.whiteElo,
+                    blackElo: game.blackElo,
+                    result: game.result,
+                    pgnHash: game.pgnHash,
+                    fewNextMove: this.findFenInPgn(game.pgn, fenHash, fenReference)
+                }
             })
-        }
+        };
+
+        const p3 = performance.now();
+
+        console.log("Total time: ", p3 - p1);
+        console.log("Searching time time: ", p2 - p1);
+        console.log("Mapping time time: ", p3 - p2);
+
+        return result;
     }
 
     async checkFen(props: CheckFenProps) {
